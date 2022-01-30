ea9d1f8c0d79259858bd6fd9b653c9256dee561e
Chicoquix <vitorfdasillva@gmail.com>
2022-01-29 21:23:08 -0300

    concluido

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -15,13 +14,36 @@ public class Main {
         curso2.setDescricao("Descrição do curso Js");
         curso2.setCargaHoraria(4);
 
+        Conteudo conteudo = new Curso();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Java Java Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devVitor = new Dev();
+        devVitor.setNome("Vitor");
+        devVitor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de Vitor"+devVitor.getConteudosInscritos());
+        devVitor.progredir();
+        System.out.println("Conteúdos concluídos de Vitor"+devVitor.getConteudosConcluidos());
+
+        Dev devChico = new Dev();
+        devVitor.setNome("Chico");
+        devChico.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de Chico"+devChico.getConteudosInscritos());
+        devChico.progredir();
+        System.out.println("Conteúdos concluídos de Chico"+devChico.getConteudosConcluidos());
     }
 }
